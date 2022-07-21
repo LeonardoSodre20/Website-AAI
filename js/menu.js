414f50ca6a45886f4e446a9a5b679962815e106b
Leonardo Vieira Sodré <LeonardoSodre25@outlook.com>
2022-07-21 00:29:26 -0300

    Menu Mobile em fase final

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,11 @@
+const buttonMenuMobile = document.querySelector(".container-menu-mobile")
 const containerLogo = document.querySelector(".container-logo")
 const imgsTransition = document.querySelectorAll('[data-img="header-imgs"]')
 let indexImage = 0
 let maxImages = imgsTransition.length
 
+
+//Código para a transição de imagens.
 function transitionImages () {
 
     imgsTransition[indexImage].classList.remove("selected")
@@ -33,4 +36,12 @@ containerLogo.addEventListener('click' , () => {
 
 window.addEventListener('load', startTransition)
 
+
+//Código para ativar o menu mobile
+
+buttonMenuMobile.addEventListener('click', () => {
+    const menuList = document.querySelector('[data-menu-list="menu-list"]')
+
+    menuList.classList.toggle('menu-list-mobile')
+})
 
